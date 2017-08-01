56d19969412d10f5439c6e8f5f99c2d53fd974cb
banupriya <banupriyajeyakodi@gmail.com>
2017-08-01 19:05:50 +0530

    win scenario

--- lib/snakesladders.rb
+++ lib/snakesladders.rb
@@ -1,32 +1,50 @@
 class SnakesLadders
 
-  attr_accessor :board, :token1, :token2, :outcome, :origin
+  attr_accessor :board, :token1, :token2, :outcome
   
   def initialize(token1, token2)
     @token1 = token1
     @token2 = token2
     @board = Array.new(10){Array.new(10).map{|arr| arr = []}}
-    @origin = Position.new(0,0)
-
-    # dice = SingleDice.new
-    # @outcome = dice.roll
-
-    # @current_token = nil
+    @snakesladders = {
+      Position.new(5,2) => Position.new(1,8), Position.new(6,8) => Position.new(3,2), Position.new(9,7) => Position.new(0,4), 
+      Position.new(4,9) => Position.new(1,1), Position.new(7,5) => Position.new(2,3), Position.new(8,8) => Position.new(6,2),
+      Position.new(0,7) => Position.new(5,6), Position.new(2,8) => Position.new(3,4), Position.new(4,2) => Position.new(7,6),
+      Position.new(1,2) => Position.new(4,4), Position.new(5,1) => Position.new(9,3), Position.new(6,5) => Position.new(8,5) }
   end 
+
+  def listener (&observer)
+    @observer = observer
+  end
   
   def move(position, outcome, token)
+    up_cell = @snakesladders.keys
+    down_cell = @snakesladders.values
     if (outcome == 1) && (check_token(token))
       current = Position.new(0, 0)  
       @board[current.x][current.y] << token
+    elsif (outcome != 1) && (check_token(token))
+      raise "#{token} can't enter into game"
     else
       current_position = Position.new(position.x, position.y)  
       token_at(position).delete_if{ |t| t == token}
       current = current_position +(outcome)
+      if (up_cell.any?{ |pos| [pos.x, pos.y] == [current.x,current.y]})
+        current = @snakesladders.select { |key,value| [key.x, key.y] == [current.x, current.y]}.values[0]
+      end
       @board[current.x][current.y] << token
+      @observer.call(WinEvent.new(token)) if (@board[9][9].include?(token))
     end
-    raise "#{token} takes another turn." if(outcome == 6)
   end
-  
+
+  def check_six(position, outcome, token, dice)
+    move(position,outcome,token)
+    if (outcome == 6)
+      current = position + outcome
+      move(current, dice, token) 
+      raise "#{token} takes another turn."
+    end
+  end
   def check_token(token)
     @board.flatten.all? { |place| place != token }
   end
@@ -35,10 +53,11 @@ class SnakesLadders
     @board[position.x][position.y]
   end
 
-  def token(position, token)
-    @board[position.x][position.y] << token
-  end
-
 end
-p @board
-p SnakesLadders.new(:X,:Y).check_token(:X)
+
+class WinEvent
+  def initialize(winner)
+    @winner = winner
+  end
+  attr_reader :winner
+end
--- spec/snakesladders_spec.rb
+++ spec/snakesladders_spec.rb
@@ -1,20 +1,25 @@
 describe 'Snakes&Ladders' do 
-    game = SnakesLadders.new(:X, :Y)
-    dice = SingleDice.new
-    let(:p0x1) {Position.new(0,1)}
-    let(:p0x4) {Position.new(0,4)}
-    let(:p0x2) {Position.new(0,2)}
-    let(:p0x0) {Position.new(0,0)}
-    let(:p3x1) {Position.new(3,1)}
-    let(:p3x5) {Position.new(3,5)}
-    let(:p4x0) {Position.new(4,0)}
-    let(:p4x6) {Position.new(4,6)}
-    let(:p3x3) {Position.new(3,3)}
-    let(:p3x6) {Position.new(3,6)}
-
-
-
-
+  game = SnakesLadders.new(:X, :Y)
+  dice = SingleDice.new
+  
+  let(:p0x1) {Position.new(0,1)}
+  let(:p0x4) {Position.new(0,4)}
+  let(:p0x6) {Position.new(0,6)}
+  let(:p1x2) {Position.new(1,2)}
+  let(:p0x2) {Position.new(0,2)}
+  let(:p0x0) {Position.new(0,0)}
+  let(:p0x3) {Position.new(0,3)}
+  let(:p3x1) {Position.new(3,1)}
+  let(:p3x5) {Position.new(3,5)}
+  let(:p4x0) {Position.new(4,0)}
+  let(:p4x6) {Position.new(4,6)}
+  let(:p3x3) {Position.new(3,3)}
+  let(:p3x6) {Position.new(3,6)}
+  let(:p5x0) {Position.new(5,0)}
+  let(:p1x8) {Position.new(1,8)}
+  let(:p1x1) {Position.new(1,1)}
+  let(:p4x4) {Position.new(4,4)}
+  let(:p9x5) {Position.new(9,5)}
 
   it 'should assign tokens to players' do 
     expect(game.token1).to eq(:X)
@@ -24,37 +29,57 @@ describe 'Snakes&Ladders' do
   it 'should players enter to board when outcome of dice one'do
     dice = double()
     dice.stub(:roll) {1}
-    game.token(p0x0, :Y)
+    count = dice.roll
+    game.move(p0x0, count, :Y)
     expect(game.token_at(p0x0)).to include(:Y)
   end
+
+  it 'should players not enter to board when outcome of dice is not one' do
+    dice = double()
+    dice.stub(:roll) {3}
+    count = dice.roll 
+    expect{game.move(p0x0, count, :X)}.to raise_error ("X can't enter into game")
+  end
   
-  it 'should token move according to outcome of event' do 
-    [2..6].each do |not_one|
-      dice = double()
-      dice.stub(:roll) { not_one }
-      game.move(p3x1, 4, :X)
-      expect(game.token_at(p3x5)).to include(:X)
-    end
+  it 'should token move according to outcome of event' do  
+    game.move(p0x0, 1, :X)
+    game.move(p0x0, 3, :X)
+    expect(game.token_at(p0x3)).to include(:X)
   end
   
   it 'should place multiple token in same cell' do
-    game.move(p0x2, 2, :X)
-    game.move(p0x1, 3, :Y)
-    expect(game.token_at(p0x4)).to include(:X, :Y)
+    game.move(p0x3, 3, :X)
+    game.move(p0x1, 5, :Y)
+    expect(game.token_at(p0x6)).to include(:X, :Y)
   end
   
   it 'should current token move twice when outcome of dice 6' do 
-    
-
-    expect{game.move(p4x0, 6, :X)}.to raise_error("X takes another turn.")
-
+    game.move(p0x0, 1, :X)
+    dice = double()
+    dice.stub(:roll) {4}
+    count = dice.roll
+    expect{game.check_six(p4x0, 6, :X, count)}.to raise_error("X takes another turn.")
+    expect(game.token_at(p5x0)).to include(:X)
   end
-  it 'should token placed on head of snake it move to tail of snake'
-  it 'should token placed on bottom of ladder it move to top of ladder'
+  
+  it 'should token placed on head of snake it move to tail of snake' do 
+    game.move(p5x0, 2, :Y)
+    expect(game.token_at(p1x8)).to include(:Y)
+  end
+  
+  it 'should token placed on bottom of ladder it move to top of ladder' do 
+    game.move(p1x1, 1, :Y)
+    expect(game.token_at(p4x4)).to include(:Y)
+  end
 
   context 'Win' do
 
-    it 'should current token placed in 100 cell'
-
+    it 'should current token placed in 100 cell' do 
+      game.listener {|event|
+      expect(event).to be_a(WinEvent)
+      expect(event.winner).to be(:X)
+      }
+      game.move(p9x5, 4, :X)
+    end
   end
 end
